c5890de48d7e659ce2942633231b6dc96999328f
Nate <natemkelley@gmail.com>
2018-02-20 13:43:44 -0700

    First commit

---
+++ db_setup.sql
@@ -0,0 +1,11 @@
+CREATE TABLE person(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fName VARCHAR(255),
+    lName VARCHAR(255),
+    mName VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(50),
+    address VARCHAR(100),
+    zip VARCHAR(10),
+    phone VARCHAR(50)
+);
