0aa71ed00817b06d8c2671f397c29ebe5dc2682a
Nate <natemkelley@gmail.com>
2018-03-01 08:26:58 -0700

    Mar 1

--- db_setup.sql
+++ db_setup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE person(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    personID Integer PRIMARY KEY AUTOINCREMENT,
     fName VARCHAR(255),
     lName VARCHAR(255),
     mName VARCHAR(255),
@@ -9,3 +9,42 @@ CREATE TABLE person(
     zip VARCHAR(10),
     phone VARCHAR(50)
 );
+
+INSERT INTO person (fName,lName,mName,city,state,address,zip,phone)
+VALUES ( 'Nate', 'Kelley', 'Michael', 'Provo', 'UT', '312 N 500 E', '84095', '385-321-9273');
+
+CREATE TABLE employee(
+    employeeID Integer PRIMARY KEY AUTOINCREMENT,
+    wage VARCHAR(255),
+    years_employed INT(100),
+    salary INT(1),
+    hourly INT(1),
+    personID VARCHAR(255),
+    FOREIGN KEY(personID) REFERENCES person(personID)
+);
+
+INSERT INTO employee (wage,years_employed,salary,hourly, personID)
+VALUES ( '30,000', '2', '0', '1', 1);
+
+CREATE TABLE dbadmin(
+    adminID Integer PRIMARY KEY AUTOINCREMENT,
+    personID VARCHAR(255),
+    FOREIGN KEY(personID) REFERENCES person(personID)
+);
+
+CREATE TABLE manager(
+    managerID Integer PRIMARY KEY AUTOINCREMENT,
+    employeeID VARCHAR(255),
+    FOREIGN KEY(employeeID) REFERENCES employee(employeeID)
+);
+
+CREATE TABLE instructor(
+    employeeID VARCHAR(255),
+    managerID VARCHAR(255),
+    FOREIGN KEY(managerID) REFERENCES manager(managerID),
+    FOREIGN KEY(employeeID) REFERENCES employee(employeeID)
+);
+
+INSERT INTO instructor
+VALUES ( 1, 2);
+
