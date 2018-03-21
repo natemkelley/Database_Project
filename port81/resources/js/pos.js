24565c1125dc7aff56f1feef7a7b60f136bdf07b
Nate <natemkelley@gmail.com>
2018-03-21 17:50:04 -0600

    swag

--- port81/resources/js/pos.js
+++ port81/resources/js/pos.js
@@ -1,1 +1,44 @@
 console.log('loading pos');
+
+
+
+function submitCust() {
+    alert('pressed');
+
+    var name = $("#fname").val();
+    var email = $("#email").val();
+    // Returns successful data submission message when the entered information is stored in database.
+
+
+
+    var dataString = 'fname=' + name + '&email1=' + email;
+
+    if (false) {
+        alert("Please Fill All Fields");
+    } else {
+        // AJAX Code To Submit Form.
+        $.ajax({
+            type: "POST",
+            url: "newCustomer.php",
+            data: dataString,
+            cache: false,
+            success: function (result) {
+                alert(result);
+            }
+        });
+    }
+};
+
+
+function submitCustomer() {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+    }
+    xmlhttp.open("GET", "newCustomer.php?" + str, true);
+    xmlhttp.send();
+
+}
